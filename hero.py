2ff86977beb8d56b888dabc8d57372bd97e60944
LadaSamoday <ladasamoday11@gmail.com>
2024-04-03 16:43:59 +0300

    :(

--- hero.py
+++ hero.py
@@ -5,12 +5,14 @@ class Hero():
         self.model.setScale(0.5)
         self.model.setPos(3, 3, 2)
         self.camera_bind()
-        self.apply_events()
         self.land = land
+        self.apply_events()
+        self.texture = 'brick.png'
     def camera_bind(self):
         base.disableMouse()
         base.camera.setH(180)
         base.camera.reparentTo(self.model)
+        base.camera.setPos(0, 0.8, 0)
 
     def turn_left(self):
         self.model.setH((self.model.getH() + 5) % 360)
@@ -32,46 +34,92 @@ class Hero():
     def move(self, x, y):
         x = self.model.getX() + x
         y = self.model.getY() + y
-        if not self.land.find_blocks((x, y, self.model.getZ())):
+        z = self.model.getZ() - 1
+        if not self.land.find_blocks((x, y, z)):
+            while not self.land.find_blocks((x, y, z - 1)):
+                z -= 1
+                if z < -10:
+                    return
+            self.model.setY(y)
+            self.model.setZ(z + 1)
+            self.model.setX(x)
+        elif not self.land.find_blocks((x, y, z + 1)):
             self.model.setY(y)
             self.model.setX(x)
-        print(self.land.find_blocks((x, y, self.model.getZ())))
+            self.model.setZ(self.model.getZ() + 1)
+
+        print(self.model.getZ())
+
+
+
 
 
     def forward(self):
-        self.check_direction(0)
+        self.move(*self.check_direction(0))
 
     def right(self):
-        self.check_direction(-90)
+        self.move(*self.check_direction(-90))
 
     def left(self):
-        self.check_direction(90)
+        self.move(*self.check_direction(90))
 
     def back(self):
-        self.check_direction(180)
+        self.move(*self.check_direction(180))
+
+    def check_pitch(self):
+        print(self.model.getP())
+        if self.model.getP() >= 270 and self.model.getP() < 340:
+            return 1
+        elif self.model.getP() < 35 or self.model.getP() > 340:
+            return 0
+        elif self.model.getP() >= 35 and self.model.getP() < 75:
+            return -1
+        else:
+            return -2
+
+    def build_block(self):
+        z = self.check_pitch()
+        x, y = self.check_direction(0)
+        self.land.add_block((self.model.getX() + x, self.model.getY() + y, self.model.getZ() + z),
+                            self.texture)
+
+
+    def break_block(self):
+        z = self.check_pitch()
+        x, y = self.check_direction(0)
+        blocks = self.land.find_blocks((self.model.getX() + x,
+                                        self.model.getY() + y, self.model.getZ() + z))
+        for block in blocks:
+            block.removeNode()
 
 
     def check_direction(self, angle):
         angle = (self.model.getH() + angle) % 360
         if angle >= 0 and angle <= 20:
-            self.move(0, -1)
+            return (0, -1)
         elif angle <= 65:
-            self.move(1, -1)
+            return (1, -1)
         elif angle <= 110:
-            self.move(1, 0)
+            return (1, 0)
         elif angle <= 155:
-            self.move(1, 1)
+            return (1, 1)
         elif angle <= 200:
-            self.move(0, 1)
+            return (0, 1)
         elif angle <= 245:
-            self.move(-1, 1)
+            return (-1, 1)
         elif angle <= 290:
-            self.move(-1, 0)
+            return (-1, 0)
         elif angle <= 335:
-            self.move(-1, -1)
+            return (-1, -1)
         else:
-            self.move(0, -1)
+            return (0, -1)
 
+    def texture_wood (self):
+        self.texture = 'wood.png'
+    def texture_stone(self):
+        self.texture = 'stone.png'
+    def texture_brick(self):
+        self.texture = 'brick.png'
 
     def apply_events(self):
         base.accept('arrow_left', self.turn_left)
@@ -90,3 +138,11 @@ class Hero():
         base.accept('a-repeat', self.left)
         base.accept('s', self.back)
         base.accept('s-repeat', self.back)
+        base.accept('space', self.build_block)
+        base.accept('b', self.break_block)
+        base.accept('1', self.texture_wood)
+        base.accept('2', self.texture_brick)
+        base.accept('3', self.texture_stone)
+        base.accept('shift-=', self.land.save_map)
+        base.accept('=', self.land.load_map)
+
--- map_manager.py
+++ map_manager.py
@@ -1,3 +1,6 @@
+import pickle
+
+
 class MapManager():
     def __init__(self):
         self.land = render.attachNewNode('Land')
@@ -10,12 +13,29 @@ class MapManager():
         self.model.setPos(pos)
         self.model.setTexture(loader.loadTexture(texture))
         self.model.setColor((0.1, 0.9, 0.6, 1))
-        self.model.setTag('pos', str(pos))
+        self.model.setTag('pos', str(tuple(map(float, pos))))
 
 
     def find_blocks(self, pos):
-        print(pos)
         return self.land.findAllMatches('=pos=' + str(pos))
+
+    def save_map(self):
+        blocks = self.land.findAllMatches('=pos=')
+        print(blocks)
+        with open('map.dat', 'wb')as file:
+            pickle.dump(len(blocks), file)
+            for block in blocks:
+                x, y, z = block.get_Pos()
+                pickle.dump((x, y, z), file)
+
+    def load_map(self):
+        with open('map.dat', 'rb')as file:
+            lenght = pickle.load(file)
+            for i in range(lenght):
+                pos = pickle.load(file)
+                self.add_block(pos, 'wood.png')
+
+
 
     def create_map(self, texture):
         with open('map.txt', 'r')as file:
@@ -27,10 +47,3 @@ class MapManager():
                     for z in range(int(n) + 1):
                         self.add_block((x, y, z), texture)
                     y += 1
-
-
-
-
-
-
-
